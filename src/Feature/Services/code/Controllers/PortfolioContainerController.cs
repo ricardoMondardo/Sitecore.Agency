95bc64e5ff7da2bac5b83ceb4fc735f24d3eb681
ricardoMondardo <ricardo9300@gmail.com>
2018-08-01 09:43:16 -0300

    Doing
    Doing

---
+++ src/Feature/Services/code/Controllers/PortfolioContainerController.cs
@@ -0,0 +1,16 @@
+﻿using Glass.Mapper.Sc.Web.Mvc;
+using Agency.Feature.Services.Models;
+using System.Web.Mvc;
+
+namespace Agency.Feature.Services.Controllers
+{
+    public class PortfolioContainerController : GlassController
+    {
+        // GET: PortfolioContainer
+        public ActionResult PortfolioContainer()
+        {
+            var data = this.GetDataSourceItem<PortfolioContainer>();
+            return View("~/Views/PortfolioContainer/PortfolioContainer.cshtml");
+        }
+    }
+}
---
+++ src/Feature/Services/code/Models/PortfolioContainer.cs
@@ -0,0 +1,14 @@
+﻿using Agency.Foundation.Orm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agency.Feature.Services.Models
+{
+    public class PortfolioContainer : SitecoreItem
+    {
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+    }
+}
