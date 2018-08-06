00cfa35bc4974e401238626322ceef9b4dd5f46e
ricardoMondardo <ricardo9300@gmail.com>
2018-08-06 19:05:59 -0300

    Doing...
    Doing...

---
+++ src/Feature/About/code/Controllers/ClientContainerController.cs
@@ -0,0 +1,20 @@
+﻿using Agency.Feature.About.Models;
+using Glass.Mapper.Sc.Web.Mvc;
+using System.Web.Mvc;
+
+namespace Agency.Feature.About.Controllers
+{
+    public class ClientContainerController : GlassController
+    {
+        public ActionResult ClientContainer()
+        {
+            return View("~/Views/ClientContainer/ClientContainer.cshtml");
+        }
+
+        public ActionResult ClientItem()
+        {
+            var data = GetDataSourceItem<ClientItem>();
+            return View("~/Views/ClientContainer/ClientItem.cshtml", data);
+        }
+    }
+}
---
+++ src/Feature/About/code/Models/ClientItem.cs
@@ -0,0 +1,10 @@
+﻿using Agency.Foundation.Orm.Models;
+using Glass.Mapper.Sc.Fields;
+
+namespace Agency.Feature.About.Models
+{
+    public class ClientItem : SitecoreItem
+    {
+        public Image Image { get; set; }
+    }
+}
