e19234ff7d882b7def41b3fc71bab4b29177e26f
ricardoMondardo <ricardo9300@gmail.com>
2018-08-07 17:25:32 -0300

    Doing...
    Doing...

---
+++ src/Feature/Media/code/Models/Footer.cs
@@ -0,0 +1,22 @@
+﻿using Agency.Foundation.Orm.Models;
+using Glass.Mapper.Sc.Configuration.Attributes;
+using Glass.Mapper.Sc.Fields;
+
+namespace Agency.Feature.Media.Models
+{
+    public class Footer : SitecoreItem
+    {
+        public Link Copyright { get; set; }
+        [SitecoreField("Link Ttwitter")]
+        public Link LinkTtwitter { get; set; }
+        [SitecoreField("Link Facebook")]
+        public Link LinkFacebook { get; set; }
+        [SitecoreField("Link Linkedin")]
+        public Link LinkLinkedin { get; set; }
+        [SitecoreField("Link Policy")]
+        public Link LinkPolicy { get; set; }
+        [SitecoreField("Link Terms")]
+        public Link LinkTerms { get; set; }
+
+    }
+}
